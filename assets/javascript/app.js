222657793a140d75b8db62aa8a1fb709079573e5
Allie Valder <avalder19@gmail.com>
2018-10-25 17:21:45 -0400

    html

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// questions 
+function check() {
+    var q1=document.myform.q1.value;
+    var q2=document.myform.q2.value;
+    var q3=document.myform.q3.value;
+    var count=0;
+}
+
+if(q1==="a"){
+    count++;
+}
+if(q1==="a"){
+    count++;
+}
+if(q1==="b"){
+    count++;
+}
+alert("you got "+count+" marks");
